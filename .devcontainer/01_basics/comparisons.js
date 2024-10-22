a05ae7e8f92e9a2a12f8b3b89941a12057f87699
Annoxentangel <annoxentangel@gmail.com>
2024-10-22 14:18:12 +0000

    comparisons.js

---
+++ .devcontainer/01_basics/comparisons.js
@@ -0,0 +1,17 @@
+console.log(2 > 1)
+console.log(2 >= 1)
+console.log(2 < 1)
+console.log(2 == 1)
+console.log(2 != 1)
+//functions normally
+
+//in comparison if first one is in string and second one is without string it functions normally
+
+//for null comparison
+console.log(null < 0)
+console.log(null > 0)
+console.log(null >= 0)
+
+// comparison convert null to a number which is 0
+// if compare with === then it also concerns with strings and same datatype
+//its importnt to campare same nos
