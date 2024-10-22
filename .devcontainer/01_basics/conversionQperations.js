9d4536763913cab99073ec47b4acaf6059815ac8
Annoxentangel <annoxentangel@gmail.com>
2024-10-22 13:25:45 +0000

    conversion Of datatypes

---
+++ .devcontainer/01_basics/conversionQperations.js
@@ -0,0 +1,27 @@
+let score = "23abc"
+
+//console.log(typeof score);
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" easily converted to 33
+//33abc they give answers NaN
+//true=> 1
+// false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1 =. true
+// 0=> false
+//"" => false
+//"iqraabdullah" => true
+
+//for string numbers
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
