f6ce1d331c03543b63194a5245e47ef435af16aa
Annoxentangel <annoxentangel@gmail.com>
2024-10-22 13:48:15 +0000

    operations for js 2

---
+++ .devcontainer/01_basics/operations.js
@@ -0,0 +1,31 @@
+let Value = 3
+let negValue = -Value
+console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2); express in power
+//console.log(2/2);
+//console.log(2%2);
+
+//for strings joining
+let str1 = "iqra"
+let str2 = " abdullah"
+let str3 = str1 + str2
+console.log(str3);
+
+//if one no is in string and second is without string then in it it combine these two digits
+//if first one is in string and second one is in no then it also combine it
+//if first two are no and third is string then it works on first two nos and combine third no with them
+//+true =>1
+
+//for different operations
+console.log((3 + 4) * 5 % 6)
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
+
+//prefix and postfix
+
